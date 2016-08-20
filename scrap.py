78e03fe8bbc9a8ffff2f8ec6ab8038391f65f3e3
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2016-08-20 09:22:38 -0500

    sfasfasf

--- scrap.py
+++ scrap.py
@@ -172,6 +172,6 @@ def log_data(source_input):
     service = build('drive', 'v2', http=http)
 
     fh = io.BytesIO(bytes(source_input))
-    rerun = insert_file(service, "{0}.txt".format(int(time.time())), "[{0}] - CyberPatriot Unofficial Scoreboard Logs".format(int(time.time())), "", "application/json", fh)
+    rerun = insert_file(service, "{0}.json".format(int(time.time())), "[{0}] - CyberPatriot Unofficial Scoreboard Logs".format(int(time.time())), "", "application/json", fh)
     print("Ending log...")
     return rerun
